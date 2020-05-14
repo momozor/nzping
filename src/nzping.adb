d3f2d4873e0217a28c746e3b550e30174263c135
Momozor <skelic3@gmail.com>
2020-05-15 03:11:22 +0800

    Change order of layout presentation

--- src/nzping.adb
+++ src/nzping.adb
@@ -15,22 +15,26 @@ begin
    
    Util.Http.Clients.Curl.Register;
    
-   Ada.Text_IO.Put_Line ("-----------------------------------------------");
-   for I in 1 .. Total_Arguments_Count loop
-      declare
-         Http     : Util.Http.Clients.Client;
-         URI      : constant String := Ada.Command_Line.Argument (I);
-         Response : Util.Http.Clients.Response;
-      begin
+   while True loop
+      Ada.Text_IO.Put_Line ("-----------------------------------------------");
+      for I in 1 .. Total_Arguments_Count loop
+         declare
+            Http     : Util.Http.Clients.Client;
+            URI      : constant String := Ada.Command_Line.Argument (I);
+            Response : Util.Http.Clients.Response;
          begin
-            Http.Get (URI, Response);
-            Ada.Text_IO.Put_Line ("|Status Code: " & Natural'Image (Response.Get_Status) & "|" & URI & "                            |");
-         exception
-            when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
-               Ada.Text_IO.Put_Line("Host not found!");
+            begin
+               Http.Get (URI, Response);
+               Ada.Text_IO.Put_Line ("|URI: " & URI & " | Status Code: " & Natural'Image (Response.Get_Status));
+            exception
+               when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
+                  Ada.Text_IO.Put_Line ("Host " & URI & " not found!");
+            end;
          end;
-      end;
+      end loop;
+      Ada.Text_IO.Put_Line ("------------------------------------------------");
+      Ada.Text_IO.Put_Line ("");
+      Ada.Text_IO.Put_Line ("Checking for another 60 seconds..");
+      delay Duration(60.0);
    end loop;
-   
-   Ada.Text_IO.Put_Line ("------------------------------------------------");
 end Nzping;
