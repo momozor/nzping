44546f9d927798210d4fb9bae60268909e68a37c
Momozor <skelic3@gmail.com>
2020-05-14 21:57:42 +0800

    Initial commit

---
+++ src/nzping.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Util.Http.Clients;
+with Util.Commands;
+
+
+procedure Nzping is
+   VERSION : String := "0.1";
+begin
+     Put_Line("Hello world! " & VERSION);
+  end Nzping;
