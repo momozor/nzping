a7d28dcef0f794787f10e8060076425dd9104b3f
Momozor <skelic3@gmail.com>
2020-05-15 02:34:51 +0800

    Add broken width

--- src/nzping.adb
+++ src/nzping.adb
@@ -4,24 +4,28 @@ with Util.Http.Clients;
 with Util.Http.Clients.Curl;
 
 procedure Nzping is
-   Count : constant Natural := Ada.Command_Line.Argument_Count;
+   TOTAL_SCREEN_WIDTH : constant Integer := 80;
+   Total_Arguments_Count : constant Natural := Ada.Command_Line.Argument_Count;
 begin
-   if Count = 0 then
-      Ada.Text_IO.Put_Line ("Usage: wget url ...");
-      Ada.Text_IO.Put_Line ("Example: wget https://www.google.com");
+   if Total_Arguments_Count = 0 then
+      Ada.Text_IO.Put_Line ("Usage: nzping url ...");
+      Ada.Text_IO.Put_Line ("Example: nzping https://twitter.com");
       return;
    end if;
+   
    Util.Http.Clients.Curl.Register;
-   for I in 1 .. Count loop
+   
+   Ada.Text_IO.Put_Line ("-----------------------------------------------");
+   for I in 1 .. Total_Arguments_Count loop
       declare
          Http     : Util.Http.Clients.Client;
-         URI      : constant String := Ada.Command_Line.Argument (1);
+         URI      : constant String := Ada.Command_Line.Argument (I);
          Response : Util.Http.Clients.Response;
       begin
-         Http.Add_Header ("X-Requested-By", "wget");
          Http.Get (URI, Response);
-         Ada.Text_IO.Put_Line ("Code: " & Natural'Image (Response.Get_Status));
-         Ada.Text_IO.Put_Line (Response.Get_Body);
+         Ada.Text_IO.Put_Line ("|Status Code: " & Natural'Image (Response.Get_Status) & "|" & URI & "                            |");
       end;
    end loop;
+   
+   Ada.Text_IO.Put_Line ("------------------------------------------------");
 end Nzping;
