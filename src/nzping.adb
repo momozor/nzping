03e23cf181cbfe9794541f02cef007d890cdae82
Momozor <skelic3@gmail.com>
2020-05-14 22:32:33 +0800

    Use WGET style

--- src/nzping.adb
+++ src/nzping.adb
@@ -1,10 +1,27 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;
+with Ada.Command_Line;
 with Util.Http.Clients;
-with Util.Commands;
-
+with Util.Http.Clients.Curl;
 
 procedure Nzping is
-   VERSION : String := "0.1";
+   Count : constant Natural := Ada.Command_Line.Argument_Count;
 begin
-     Put_Line("Hello world! " & VERSION);
-  end Nzping;
+   if Count = 0 then
+      Ada.Text_IO.Put_Line ("Usage: wget url ...");
+      Ada.Text_IO.Put_Line ("Example: wget https://www.google.com");
+      return;
+   end if;
+   Util.Http.Clients.Curl.Register;
+   for I in 1 .. Count loop
+      declare
+         Http     : Util.Http.Clients.Client;
+         URI      : constant String := Ada.Command_Line.Argument (1);
+         Response : Util.Http.Clients.Response;
+      begin
+         Http.Add_Header ("X-Requested-By", "wget");
+         Http.Get (URI, Response);
+         Ada.Text_IO.Put_Line ("Code: " & Natural'Image (Response.Get_Status));
+         Ada.Text_IO.Put_Line (Response.Get_Body);
+      end;
+   end loop;
+end Nzping;
