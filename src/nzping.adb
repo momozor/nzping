444a48523eacf228c31fc763105ca8e2e5241a95
Momozor <skelic3@gmail.com>
2020-05-17 00:53:01 +0800

    Raise exception if wrong argument is given for time

--- src/nzping.adb
+++ src/nzping.adb
@@ -29,64 +29,72 @@ begin
       return;
    end if;
    
-   Util.Http.Clients.Curl.Register;
-   
-   Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
-   Text_IO.New_Line;
-   
-   Recheck_Interval_Seconds := Float'Value (Ada.Command_Line.Argument (1));
-   
-   loop
-      for I in 2 .. Total_Arguments_Count loop
-         declare
-            Http     : Util.Http.Clients.Client;
-            URI      : constant String := Ada.Command_Line.Argument (I);
-            Response : Util.Http.Clients.Response;
-            Status_Code : Natural;
-            Status_Type : Unbounded.Unbounded_String;
-         begin
+   begin
+      Recheck_Interval_Seconds := 
+        Float'Value (Ada.Command_Line.Argument (1));
+      
+   exception
+      when Error : CONSTRAINT_ERROR =>
+         Text_IO.Put_Line ("[ERROR] interval time must be in floating-point or integer!");
+         return;
+   end;
+      
+         Util.Http.Clients.Curl.Register;
+         
+         Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+         Text_IO.New_Line;
+         
+         loop
+         for I in 2 .. Total_Arguments_Count loop
+            declare
+               Http     : Util.Http.Clients.Client;
+               URI      : constant String := Ada.Command_Line.Argument (I);
+               Response : Util.Http.Clients.Response;
+               Status_Code : Natural;
+               Status_Type : Unbounded.Unbounded_String;
             begin
-               Http.Get (URI, Response);
-               Status_Code := Response.Get_Status;
-               
-               case Status_Code is
-                  when 200 .. 399 =>
-                     Status_Type := Unbounded.To_Unbounded_String ("SUCCESS");
-                  when others =>
-                     Status_Type := Unbounded.To_Unbounded_String ("ERROR");
-               end case;
-               
-               Text_IO.Put_Line ("URI: " 
-                                   & URI
-                                   & " | Status Type: " 
-                                   & Unbounded.To_String (Status_Type)
-                                   & " | Status Code: "
-                                   & Natural'Image (Status_Code)
-                                );
-            exception
-               when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
-                  Text_IO.Put_Line ("Host " & URI & " not found!");
+               begin
+                  Http.Get (URI, Response);
+                  Status_Code := Response.Get_Status;
                   
-                  Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+                  case Status_Code is
+                     when 200 .. 399 =>
+                        Status_Type := Unbounded.To_Unbounded_String ("SUCCESS");
+                     when others =>
+                        Status_Type := Unbounded.To_Unbounded_String ("ERROR");
+                  end case;
                   
-                  Text_IO.New_Line;
-                  return;
-                  
+                  Text_IO.Put_Line ("URI: " 
+                                      & URI
+                                      & " | Status Type: " 
+                                      & Unbounded.To_String (Status_Type)
+                                      & " | Status Code: "
+                                      & Natural'Image (Status_Code)
+                                   );
+               exception
+                  when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
+                     Text_IO.Put_Line ("Host " & URI & " not found!");
+                     
+                     Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+                     
+                     Text_IO.New_Line;
+                     return;
+                     
+               end;
             end;
-         end;
+         end loop;
+         
+         Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+         Text_IO.New_Line;
+         
+         Text_IO.Put ("Checking for another");
+         Float_Text_IO.Put (Recheck_Interval_Seconds,
+                            Fore => 5,
+                            Aft => 2,
+                            Exp => 0);
+         Text_IO.Put (" second(s)..");
+         Text_IO.New_Line;
+         
+         delay Duration (Recheck_Interval_Seconds);
       end loop;
-      
-      Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
-      Text_IO.New_Line;
-      
-      Text_IO.Put ("Checking for another");
-      Float_Text_IO.Put (Recheck_Interval_Seconds,
-                         Fore => 5,
-                         Aft => 2,
-                         Exp => 0);
-      Text_IO.Put (" second(s)..");
-      Text_IO.New_Line;
-      
-      delay Duration (Recheck_Interval_Seconds);
-   end loop;
 end Nzping;
