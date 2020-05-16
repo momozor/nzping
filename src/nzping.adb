80b1e243fcb329919691bb68284182445da58cd8
Momozor <skelic3@gmail.com>
2020-05-17 02:02:28 +0800

    Handle wrong link input

--- src/nzping.adb
+++ src/nzping.adb
@@ -2,6 +2,7 @@ with Ada.Text_IO;
 with Ada.Float_Text_IO;
 with Ada.Command_Line;
 with Ada.Strings.Unbounded;
+with Ada.Strings.Fixed;
 with Util.Http.Clients;
 with Util.Http.Clients.Curl;
 
@@ -9,6 +10,7 @@ procedure Nzping is
    package Text_IO renames Ada.Text_IO;
    package Float_Text_IO renames Ada.Float_Text_IO;
    package Unbounded renames Ada.Strings.Unbounded;
+   package Fixed renames Ada.Strings.Fixed;
    
    procedure Print_Border_Lines (Lines_Length: in Integer) is
    begin
@@ -38,63 +40,72 @@ begin
          Text_IO.Put_Line ("[ERROR] interval time must be in floating-point or integer!");
          return;
    end;
-      
-         Util.Http.Clients.Curl.Register;
-         
-         Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
-         Text_IO.New_Line;
-         
-         loop
-         for I in 2 .. Total_Arguments_Count loop
-            declare
-               Http     : Util.Http.Clients.Client;
-               URI      : constant String := Ada.Command_Line.Argument (I);
-               Response : Util.Http.Clients.Response;
-               Status_Code : Natural;
-               Status_Type : Unbounded.Unbounded_String;
+   
+   Util.Http.Clients.Curl.Register;
+   
+   Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+   Text_IO.New_Line;
+   
+   loop
+      for I in 2 .. Total_Arguments_Count loop
+         declare
+            Http     : Util.Http.Clients.Client;
+            URI      : constant String := Ada.Command_Line.Argument (I);
+            Response : Util.Http.Clients.Response;
+            Status_Code : Natural;
+            Status_Type : Unbounded.Unbounded_String;
+         begin
+            
             begin
-               begin
+               
+               if Fixed.Index (URI, "http") > 0
+                 or Fixed.Index (URI, "https") > 0 then
+                  
                   Http.Get (URI, Response);
-                  Status_Code := Response.Get_Status;
+               else
+                  Text_IO.Put_Line ("[ERROR] Link must start with http or https!!!");
+                  return;
+               end if;
+               
+               Status_Code := Response.Get_Status;
+               
+               case Status_Code is
+                  when 200 .. 399 =>
+                     Status_Type := Unbounded.To_Unbounded_String ("SUCCESS");
+                  when others =>
+                     Status_Type := Unbounded.To_Unbounded_String ("ERROR");
+               end case;
+               
+               Text_IO.Put_Line ("URI: " 
+                                   & URI
+                                   & " | Status Type: " 
+                                   & Unbounded.To_String (Status_Type)
+                                   & " | Status Code: "
+                                   & Natural'Image (Status_Code));
+            exception
+               when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
+                  Text_IO.Put_Line ("Host " & URI & " not found!");
                   
-                  case Status_Code is
-                     when 200 .. 399 =>
-                        Status_Type := Unbounded.To_Unbounded_String ("SUCCESS");
-                     when others =>
-                        Status_Type := Unbounded.To_Unbounded_String ("ERROR");
-                  end case;
+                  Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
                   
-                  Text_IO.Put_Line ("URI: " 
-                                      & URI
-                                      & " | Status Type: " 
-                                      & Unbounded.To_String (Status_Type)
-                                      & " | Status Code: "
-                                      & Natural'Image (Status_Code)
-                                   );
-               exception
-                  when Error : UTIL.HTTP.CLIENTS.CONNECTION_ERROR  =>
-                     Text_IO.Put_Line ("Host " & URI & " not found!");
-                     
-                     Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
-                     
-                     Text_IO.New_Line;
-                     return;
-                     
-               end;
+                  Text_IO.New_Line;
+                  return;
+                  
             end;
-         end loop;
-         
-         Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
-         Text_IO.New_Line;
-         
-         Text_IO.Put ("Checking for another");
-         Float_Text_IO.Put (Recheck_Interval_Seconds,
-                            Fore => 5,
-                            Aft => 2,
-                            Exp => 0);
-         Text_IO.Put (" second(s)..");
-         Text_IO.New_Line;
-         
-         delay Duration (Recheck_Interval_Seconds);
+         end;
       end loop;
+      
+      Print_Border_Lines (DEFAULT_TOTAL_SCREEN_WIDTH);
+      Text_IO.New_Line;
+      
+      Text_IO.Put ("Checking for another");
+      Float_Text_IO.Put (Recheck_Interval_Seconds,
+                         Fore => 5,
+                         Aft => 2,
+                         Exp => 0);
+      Text_IO.Put (" second(s)..");
+      Text_IO.New_Line;
+      
+      delay Duration (Recheck_Interval_Seconds);
+   end loop;
 end Nzping;
