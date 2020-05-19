41e0105e7469512cdd756a5d2e9922c3eefd1f04
Momozor <skelic3@gmail.com>
2020-05-19 14:40:10 +0800

    Add Print_Current_Time

--- src/utilities.adb
+++ src/utilities.adb
@@ -1,7 +1,11 @@
 with Ada.Text_IO;
+with Ada.Calendar;
+with Ada.Calendar.Formatting;
 
 package body Utilities is
    package Text_IO renames Ada.Text_IO;
+   package Calendar renames Ada.Calendar;
+   package Calendar_Formatting renames Ada.Calendar.Formatting;
 
    procedure Print_Border_Lines (Lines_Length : Integer) is
    begin
@@ -9,5 +13,11 @@ package body Utilities is
          Text_IO.Put ("-");
       end loop;
    end Print_Border_Lines;
+
+   procedure Print_Current_Time is
+      Now : constant Calendar.Time := Calendar.Clock;
+   begin
+      Text_IO.Put_Line ("[ " & Calendar_Formatting.Image (Now) & " ]");
+   end Print_Current_Time;
 
 end Utilities;
--- src/utilities.ads
+++ src/utilities.ads
@@ -1,3 +1,4 @@
 package Utilities is
    procedure Print_Border_Lines (Lines_Length : Integer);
+   procedure Print_Current_Time;
 end Utilities;
